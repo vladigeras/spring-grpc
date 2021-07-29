38c707abe60281510fce113fbe9f59e181cadf4a
Vladislav Gerasimov <gerasimov.v88@gmail.com>
2021-07-29 11:16:02 +0300

    Initial commit

---
+++ src/test/java/ru/vladigeras/springgrpc/SpringGrpcApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vladigeras.springgrpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGrpcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
